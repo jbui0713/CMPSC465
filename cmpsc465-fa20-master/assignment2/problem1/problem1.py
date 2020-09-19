a5b2eb78b77a130eac1d11ca04ff87cf4f0da9fa
ngehring <ngehring33@gmail.com>
2020-09-19 18:01:27 -0400

    Added py file

---
+++ cmpsc465-fa20-master/assignment2/problem1/problem1.py
@@ -0,0 +1,15 @@
+
+
+#array_A = list(map(int, input().strip().split()))
+#print(array_A)
+
+
+array_B = list(map(int, input().strip().split()))
+print(array_B)
+pairs = 0
+for i in range(len(array_B)):
+    for j in range(i+1, len(array_B)):
+        if (array_B[i] > array_B[j]):
+            pairs+=1
+print(pairs)
+
