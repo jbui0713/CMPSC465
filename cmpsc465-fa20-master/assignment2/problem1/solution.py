f1677a543701eacfe6bc8b39e51ec6243f3f0710
ngehring <ngehring33@gmail.com>
2020-09-19 18:11:22 -0400

    commented print statement

--- cmpsc465-fa20-master/assignment2/problem1/solution.py
+++ cmpsc465-fa20-master/assignment2/problem1/solution.py
@@ -4,7 +4,7 @@ array_A = list(map(int, input().strip().split()))
 #print(array_A)
 
 array_B = list(map(int, input().strip().split()))
-print(array_B)
+#print(array_B)
 pairs = 0
 for i in range(len(array_B)):
     for j in range(i+1, len(array_B)):
