92a645f513f0c95bb5f3674350ef7ed59affbeee
jbui0713 <59713200+jbui0713@users.noreply.github.com>
2020-09-22 19:32:12 -0400

    Updated solution

--- cmpsc465-fa20-master/assignment2/problem2/solution.py
+++ cmpsc465-fa20-master/assignment2/problem2/solution.py
@@ -1,4 +1,5 @@
 import math
+#Thanks to Matt, he helped us solve this algorithim out. We love you <3
 
 def convexHull(points):
     n = len(points)
@@ -7,20 +8,15 @@ def convexHull(points):
     lUpper = [points[0], points[1]]
     for i in range(2, len(points)):
         lUpper.append(points[i])
-       # print(str(lUpper[-3:]))
         while(len(lUpper) > 2 and turningLeft(lUpper[-3:])):
             del lUpper[-2]
 
     lLower = [points[n-1],points[n-2]]
-    for i in range(len(points)-3, 0, -1):
+    for i in range(len(points)-3, -1, -1):
         lLower.append(points[i])
         while(len(lLower)>2 and turningLeft(lLower[-3:])):
             del lLower[-2]
-    del lLower[0]
-    del lLower[-1]
-    print(lUpper)
-    print(lLower)
-    # for
+    print(len(lLower),len(lUpper))
 
 def combine():
     pass
@@ -43,8 +39,17 @@ def merge(a, b):
     mergedArray = []
     while((currentA < len(a)) & (currentB < len(b))):
         ai = a[currentA][0]
-        bj = b[currentB][0]
-        if (ai<=bj):
+        bi = b[currentB][0]
+        aj = a[currentA][1]
+        bj = b[currentB][1]
+        if (ai==bi):
+            if (aj<=bj):
+                mergedArray.append(a[currentA])
+                currentA += 1
+            else:
+                mergedArray.append(b[currentB])
+                currentB += 1
+        elif (ai<bi):
             mergedArray.append(a[currentA])
             currentA += 1
         else:
@@ -68,13 +73,12 @@ def sortAndCount(l):
         l = merge(a,b)
     return (l)
 
-#turningLeft([[0,0],[0,2],[-1,0]])
-#
 numPoints = list(map(int, input().strip().split()))
 points = []
 
 for i in range(numPoints[0]):
-    point = list(map(float, input().strip().split()))
+    line = list(map(float, input().strip().split()))
+    point = [line[0], -line[1]]
     points.append(point)
 convexHull(sortAndCount(points))
-# print(sortAndCount(points))
+
